2193818f47a971d5ddfd8ec0ae9130dd4d1173ee
Timothy Dietsch <tmdietsch56@gmail.com>
2017-09-12 21:39:19 -0500

    Explanations
    Details explaining what a constant, a variable, the difference between a constant and a variable, and data types

--- Week_2.groovy
+++ Week_2.groovy
@@ -1,11 +1,15 @@
 println "Hello World"
-
+// A constant is something, such as a string, that does not change
+/** A data type classifies the data so
+ that the compiler knows what to do with it **/
 String firstName = "Timothy"
 String lastName = "Dietsch"
-
+/** A variable is something, like an integer,
+ that can be change as many times as necessary **/
 Integer elite = 1337
 Integer notAsElite = elite / 2
-
+/** A variable can be changed,
+ a constant can not change **/
 println elite * 2
 println notAsElite + (elite / 3)
 println notAsElite - 400
